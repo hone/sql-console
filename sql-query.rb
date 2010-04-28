a3bdbb4a2bb489ed7c6a05595e38437e3acaa6be
David Dollar <ddollar@gmail.com>
2010-04-28 12:24:00 -0700

    handle errors

--- sql-query.rb
+++ sql-query.rb
@@ -7,13 +7,20 @@ require 'ruport'
 
 class SqlQuery < Sinatra::Base
   get '/query' do
-    db = Sequel.connect(params[:database_url])
-    query = db[params[:sql]]
-    columns = query.columns
-    data = query.collect do |row|
-      columns.collect {|column| row[column] }
-    end
 
-    Table(:data => data, :column_names => columns).as(:text, :ignore_table_width => true)
+    begin
+      db      = Sequel.connect(params[:database_url])
+      query   = db[params[:sql]]
+      columns = query.columns
+
+      data = query.collect do |row|
+        columns.collect {|column| row[column] }
+      end
+
+      Table(:data => data, :column_names => columns).as(:text, :ignore_table_width => true)
+
+    rescue Sequel::DatabaseError => ex
+      ex.message
+    end
   end
 end
