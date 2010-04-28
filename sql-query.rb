02905687560d88d760ef8dfc8de7d895042c5871
David Dollar <ddollar@gmail.com>
2010-04-28 12:40:29 -0700

    fix twice-running queries

--- sql-query.rb
+++ sql-query.rb
@@ -11,13 +11,15 @@ class SqlQuery < Sinatra::Base
     begin
       db      = Sequel.connect(params[:database_url])
       query   = db[params[:sql]]
+
+      rows    = query.all
       columns = query.columns
 
-      data = query.collect do |row|
-        columns.collect {|column| row[column] }
+      data = rows.collect do |row|
+        columns.collect { |column| row[column] }
       end
 
-      Table(:data => data, :column_names => columns).as(:text, :ignore_table_width => true)
+      Table(:data => data, :column_names => data).as(:text, :ignore_table_width => true)
 
     rescue Sequel::DatabaseError => ex
       ex.message
