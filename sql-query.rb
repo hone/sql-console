8225162e384f42464208801db23e92f6ab7b5a73
Terence Lee <hone02@gmail.com>
2010-04-29 13:19:22 -0500

    upgrade message

--- sql-query.rb
+++ sql-query.rb
@@ -7,26 +7,6 @@ require 'ruport'
 
 class SqlQuery < Sinatra::Base
   get '/query' do
-
-    begin
-      db      = Sequel.connect(params[:database_url])
-      query   = db[params[:sql]]
-
-      rows    = query.all
-      columns = query.columns
-
-      data = rows.collect do |row|
-        columns.collect { |column| row[column] }
-      end
-
-      if columns.length > 0
-        Table(:data => data, :column_names => columns).as(:text, :ignore_table_width => true)
-      else
-        "OK"
-      end
-
-    rescue Sequel::DatabaseError => ex
-      ex.message
-    end
+    "Please upgrade your sql-console plugin"
   end
 end
