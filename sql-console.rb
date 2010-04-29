1a8c54cfc76075a1f7d0cf4b7c05f2ef4ea34072
Terence Lee <hone02@gmail.com>
2010-04-29 13:25:30 -0500

    change get to post

--- sql-console.rb
+++ sql-console.rb
@@ -9,4 +9,27 @@ class SqlConsole < Sinatra::Base
   get '/query' do
     "Please upgrade your sql-console plugin"
   end
+
+  post '/query' do
+    begin
+      db      = Sequel.connect(params[:database_url])
+      query   = db[params[:sql]]
+
+      rows    = query.all
+      columns = query.columns
+
+      data = rows.collect do |row|
+        columns.collect { |column| row[column] }
+      end
+
+      if columns.length > 0
+        Table(:data => data, :column_names => columns).as(:text, :ignore_table_width => true)
+      else
+        "OK"
+      end
+
+    rescue Sequel::DatabaseError => ex
+      ex.message
+    end
+  end
 end
