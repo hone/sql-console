49eeee56476484c09af05ade96fa2d626902a4e2
Terence Lee <hone02@gmail.com>
2010-05-01 04:13:18 -0500

    describe tables

--- sql-console.rb
+++ sql-console.rb
@@ -20,6 +20,21 @@ class SqlConsole < Sinatra::Base
       if /^\s*show tables\s*$/i =~ params[:sql]
         columns = [:table_name]
         data = db.tables.collect {|table| [table.to_s] }
+      elsif match_data = params[:sql].match(/^\s*describe (\w+)\s*/)
+        schema  = db.schema(match_data[1])
+        begin
+          columns = ["Column", "Type", "Modifiers"]
+          data = schema.collect do |column|
+            hash = column.last
+            modifiers = []
+            modifiers << "primary key" if hash[:primary_key]
+            modifiers << "not null" unless hash[:allow_null]
+            modifiers << hash[:default] if hash[:default]
+            [column.first, hash[:db_type], modifiers.join(" ")]
+          end
+        rescue Sequel::Error
+          "Not a valid table name"
+        end
       else
         query   = db[params[:sql]]
         rows    = query.all
